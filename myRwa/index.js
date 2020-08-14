73dc61e1b3660938f413ef878835037698896abc
Rachit Khurana <rachitkay@icloud.com>
2020-08-14 20:13:41 +0530

    myRWA more posts, image post, side panel, top mobile panel

---
+++ myRwa/index.js
@@ -0,0 +1,10 @@
+// for thank button
+function clickme() {
+    document.getElementById("thankBtnImg").classList.toggle("likeLiked");
+    document.getElementById("thankBtnImg").classList.toggle("likeDefault");
+}
+
+// for make post button
+function makePost() {
+    
+}
