c8357f2d9af0071f55739eee8900a238eb476884
Rachit Khurana <rachitkay@icloud.com>
2020-08-15 22:14:28 +0530

    myRWA makePost popup, icon changes, rightPanel changes, searchBar, advert 3 to 1, like-disliks, alignment changes,

--- myRwa/index.js
+++ myRwa/index.js
@@ -1,10 +1,1 @@
 // for thank button
-function clickme() {
-    document.getElementById("thankBtnImg").classList.toggle("likeLiked");
-    document.getElementById("thankBtnImg").classList.toggle("likeDefault");
-}
-
-// for make post button
-function makePost() {
-    
-}
