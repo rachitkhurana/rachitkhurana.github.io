e0b36a20315d6e0bbded3c5ea3a66bd280c7f75a
Rachit Khurana <rachitkay@icloud.com>
2020-08-17 23:43:25 +0530

    myRwa, layout changes, color changes, reply, image comment, side menu change

--- myRwa/index.js
+++ myRwa/index.js
@@ -1,1 +1,21 @@
-// for thank button
+console.log('Online & Ready Sirr!');
+
+
+$('.alert').hide();
+
+function errorAlert(input) {
+    $('#errorAlertSpan').html(input);
+    $('#errorAlert').show();
+    setTimeout(() => {
+        $('#errorAlert').hide();
+    }, 3000);
+}
+
+
+function successAlert(input) {
+    $('#successAlertSpan').html(input);
+    $('#successAlert').show();
+    setTimeout(() => {
+        $('#successAlert').hide();
+    }, 3000);
+}
